0035b62ef37c2c9030591cf22ef13e7126b95acb
matiaspan <matiaspan@2dcc1c5e-6ac2-6f9f-833e-70b64b0a047f>
2011-02-17 00:34:11 +0000

    Game Instance Manager

---
+++ trunk/src/mygame/model/PickupGameInstanceManager.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mygame.model;
+
+/**
+ *
+ * @author matiaspan
+ */
+public class PickupGameInstanceManager implements GameInstanceManager {
+
+    private int energy;
+    private float score;
+
+    public boolean isGameOver() {
+        return energy <= 0;
+    }
+
+    public float getScore() {
+        return score;
+    }
+
+    public int getEnergyLeft() {
+        return energy;
+    }
+
+    public void damageCharacter(float damage) {
+        energy -= damage;
+    }
+
+    public void addPoints(float points) {
+        score += points;
+    }
+
+}
